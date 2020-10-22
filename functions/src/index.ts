1be2d50a6c7bbf11b72c2eabc05b8a317db63a68
HYUNWOO KIM <stories282@gmail.com>
2020-10-23 02:18:38 +0900

    bike data show

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,24 @@
 import * as functions from 'firebase-functions';
+import axios from 'axios';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+export const bike = functions.region('asia-northeast1').https.onRequest((request, response) => {
+  response.set('Access-Control-Allow-Origin', '*');
+  axios.get('http://bike.jejusi.go.kr/map/MapData.json',
+  )
+  .then(function (res) {
+    response.send({
+      success: true,
+      data: res.data
+    });
+  })
+  .catch(function (error) {
+    functions.logger.info(`Failed to get bike data: ${error.message}`, {structuredData: true});
+    response.send({
+      success: false,
+      message: error.message
+    });
+  })
 });
